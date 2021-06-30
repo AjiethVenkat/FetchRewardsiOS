5dd7a1d6f784b317cbb35ba7fb1d2b014572d0ff
Ajieth <Ajiethvenkat@gmail.com>
2021-06-29 20:09:01 -0400

    Initial Commit

---
+++ FetchRewards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchRewards
+//
+//  Created by Cibi Balachander on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
